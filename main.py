13c0827b0e9e965b757c6b108a5541fa258ea48f
lastwhisper <lastwhisper98@gmail.com>
2023-11-25 21:05:58 -0600

    update

--- main.py
+++ main.py
@@ -23,30 +23,32 @@ def root():
     return 'Hello World!'
 
 
-@app.get("/{user_id}/{concert_id}")
 async def main(user_id: str, concert_id: str):
+    async def task_wrapper(task: str, coro: any):
+        return task, await coro
+
     tasks = [
-        get_user_info(user_id),
-        get_user_songs(user_id),
-        get_concert_info(concert_id),
-        get_user_matches(user_id, concert_id)
+        task_wrapper('info', get_user_info(user_id)),
+        task_wrapper('songs', get_user_songs(user_id)),
+        task_wrapper('concert', get_concert_info(concert_id)),
+        task_wrapper('matches', get_user_matches(user_id, concert_id))
     ]
 
     results = {}
-    for task in asyncio.as_completed(tasks):
-        result = await task
-        if isinstance(result, UserInfo):  # Assuming get_user_info returns an instance of UserInfo
+    for future in asyncio.as_completed(tasks):
+        name, result = await future
+        if name == 'info':
             results['info'] = parse_user_info(result)
-        elif isinstance(result, list):  # Assuming get_user_songs returns a list
-            # You may need to refine this condition based on the actual return type
+        elif name == 'songs':
             results['songs'] = parse_user_songs(result)
-        elif isinstance(result, Concert):  # Assuming get_concert_info returns an instance of Concert
+        elif name == 'concert':
             results['concert'] = parse_concert_info(result)
-        elif isinstance(result, Match):  # Assuming get_user_matches returns an instance of Match
+        elif name == 'matches':
             results['matches'] = parse_user_matches(result)
-        # Add additional conditions as necessary for different return types
 
+    print(results)
     return results
+
 
 
 # =====================================
@@ -162,6 +164,7 @@ async def get_concert_info(concert_id: str):
 
     async with httpx.AsyncClient() as client:
         try:
+            await asyncio.sleep(random.random())
             response = await client.get(url)
 
             if response.status_code == 200:
@@ -202,6 +205,7 @@ async def get_user_matches(user_id: str, concert_id: str):
 
     async with httpx.AsyncClient() as client:
         try:
+            await asyncio.sleep(random.random())
             response = await client.post(url, data={'userId': user_id, 'concertId': concert_id})
 
             if response.status_code == 200:
